38d2e2e36e78c6dd28087246e13b11ba83fa1b3f
h2svolt <info.h2svolt@gmail.com>
2026-09-03 18:54:05 +0500

    Make K222 APK compatible with older Android and enable v1/v2 signing

--- android-wrapper/app/build.gradle.kts
+++ android-wrapper/app/build.gradle.kts
@@ -8,13 +8,25 @@ android {
 
     defaultConfig {
         applicationId = "com.k222game.app"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            enableV1Signing = true
+            enableV2Signing = true
+            enableV3Signing = true
+            enableV4Signing = false
+        }
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             isMinifyEnabled = false
         }
