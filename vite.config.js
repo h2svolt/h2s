c952a3b56964bb870603646ac018dedcf5341759
h2svolt <info.h2svolt@gmail.com>
2026-09-26 19:54:58 +0500

    Deploy Infiniti Games client concept

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  publicDir: 'Public',   // <-- point Vite at your capital-P folder
-})
